0318c17f537d83d5a601c6413afc8c2b0f2a5e1d
ANine <codergangluo@126.com>
2014-06-30 15:31:52 +0800

    *.jenkins test

---
+++ jenkinsTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  jenkinsTest
+//
+//  Created by ANine on 6/30/14.
+//  Copyright (c) 2014 ANine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ jenkinsTestTests/jenkinsTestTests.m
@@ -0,0 +1,34 @@
+//
+//  jenkinsTestTests.m
+//  jenkinsTestTests
+//
+//  Created by ANine on 6/30/14.
+//  Copyright (c) 2014 ANine. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface jenkinsTestTests : XCTestCase
+
+@end
+
+@implementation jenkinsTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
