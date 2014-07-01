8fa99c749ddc44ab918817e1e51cc39b385e162a
ANine <codergangluo@126.com>
2014-07-01 21:25:41 +0800

    *.又来嘛

--- jenkinsTest/AppDelegate.m
+++ jenkinsTest/AppDelegate.m
@@ -18,7 +18,7 @@
     
     
     
-    NSLog(@"8测试自动化构建,commtiTime: %@",[NSDate date]);
+    NSLog(@"9测试自动化构建,commtiTime: %@",[NSDate date]);
     //do some change.
 //    NSLog(@"error")
     self.window.backgroundColor =	 [UIColor whiteColor];
